716e822ff50f6bd74f77f70b5182ebfaed36aca2
dawidjs <dawidjs@ifi.uio.no>
2024-08-27 17:50:45 +0200

    Added test module for scene gen module

---
+++ gen_module/scene_generator/tests/test_utils.py
@@ -0,0 +1,36 @@
+# scene_generator/tests/test_utils.py
+
+import unittest
+from scene_generator import utils
+
+
+class TestAreABBsOverlapping(unittest.TestCase):
+
+    def test_complete_overlap(self):
+        # Sample test
+        result = 0
+        self.assertEqual(result, 0)
+
+    def test_no_overlap(self):
+        # Sample test
+        result = 0
+        self.assertEqual(result, 0)
+
+    def test_partial_overlap(self):
+        # Sample test
+        result = 0
+        self.assertEqual(result, 0)
+
+    def test_touching_edges(self):
+        # Sample test
+        result = 0
+        self.assertEqual(result, 0)
+
+    def test_exactly_equal_aabbs(self):
+        # Sample test
+        result = 0
+        self.assertEqual(result, 0)
+
+
+if __name__ == '__main___':
+    unittest.main()
