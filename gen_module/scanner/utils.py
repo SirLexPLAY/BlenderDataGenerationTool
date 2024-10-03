46e8eb8c4d1d98ecbe983a90c7149d69ccf913c7
dawidjs <dawidjs@ifi.uio.no>
2024-10-03 15:15:10 +0200

    Defined camera_setup() function + minor changes

--- gen_module/scanner/utils.py
+++ gen_module/scanner/utils.py
@@ -2,10 +2,11 @@
 
 import bpy
 import blensor
+import random
 from math import pi
 
 
-def keyframe_setup(scanner_object, frame_start, frame_end, angle_start, angle_end):
+def keyframe_setup(scanner_object, frame_start, frame_end, min_angle, max_angle):
     """
     #TODO: needs to be properly documented
 
@@ -15,28 +16,40 @@ def keyframe_setup(scanner_object, frame_start, frame_end, angle_start, angle_en
     """
     scanner_object.animation_data_clear()
 
-    scanner_object.rotation_euler = (angle_start, 0, 0)
+    scanner_object.rotation_euler = (min_angle/180*pi, 0, 0)
     scanner_object.keyframe_insert(data_path="rotation_euler", frame=frame_start)
 
-    scanner_object.rotation_euler = (angle_end, 0, 0)
+    scanner_object.rotation_euler = (max_angle/180*pi, 0, 0)
     scanner_object.keyframe_insert(data_path="rotation_euler", frame=frame_end)
 
 
-def scan_range(scanner, frame_start, frame_end, add_blender_mesh=False, add_noisy_blender_mesh=False):
+def camera_setup(scanner_object: bpy.types.Object, scene_size: float):
+    location = [
+                random.random()*scene_size - scene_size/2,  # x
+                random.random()*scene_size - scene_size/2,  # y
+                random.random()*scene_size - scene_size/2   # z
+            ]
+    
+    scanner_object.location = location
+
+
+def scan_range(scanner_object, frame_start, frame_end, add_blender_mesh=False, add_noisy_blender_mesh=False):
     """
     #TODO: needs to be properly documented
 
     Performs the scan, but it should be considered to remove this method if it
     only calls a method passing the parameters directly without any modification on them.
     """
+    scanner_object.velodyne_model = "vlp16"
     
     blensor.blendodyne.scan_range(
-        scanner_object=scanner,
+        scanner_object=scanner_object,
         frame_start=frame_start,
         frame_end=frame_end,
         add_blender_mesh=add_blender_mesh,
-        add_noisy_blender_mesh=add_noisy_blender_mesh
-    )
+        add_noisy_blender_mesh=add_noisy_blender_mesh,
+        world_transformation=scanner_object.matrix_world,
+    ) 
 
 
 def render_image(filepath, fileformat, engine="CYCLES"):
