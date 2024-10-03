00be6e23fe3ccc56689693cc33acf3c372d68a4b
dawidjs <dawidjs@ifi.uio.no>
2024-10-03 15:14:10 +0200

    Defined ScannerModule class and scan_scene method

--- gen_module/scanner/main.py
+++ gen_module/scanner/main.py
@@ -1,1 +1,27 @@
 # scanner/main.py
+import bpy
+from scanner.utils import keyframe_setup, camera_setup, scan_range
+from scanner.scanner_params import ScannerParams
+
+
+class ScannerModule:
+
+    def scan_scene(self, scanner_params: ScannerParams, numer_of_scans: int = 1):
+        camera_setup(
+            scanner_params.scanner_object, 
+            scanner_params.scene_size
+            )
+        keyframe_setup(
+            scanner_params.scanner_object, 
+            scanner_params.frame_start, 
+            scanner_params.frame_end, 
+            scanner_params.max_angle, 
+            scanner_params.min_angle
+            )
+        scan_range(
+            scanner_params.scanner_object, 
+            scanner_params.frame_start, 
+            scanner_params.frame_end,
+            add_noisy_blender_mesh=scanner_params.add_noisy_blender_mesh
+            )
+        
